9a71da5c3201f54f21e4e933ae613d8242aa42cc
ShahramShakiba <shahramshakiba88@gmail.com>
2024-06-22 19:27:27 +0430

    Create 1000 Particles

--- src/script.js
+++ src/script.js
@@ -2,12 +2,46 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import GUI from 'lil-gui';
 import * as THREE from 'three';
 
-//===================== Objects ======================
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
-);
-scene.add(cube);
+const canvas = document.querySelector('canvas.webgl');
+const scene = new THREE.Scene();
+const gui = new GUI().title('Galaxy Generator');
+
+//====================== Galaxy =======================
+const parameters = {};
+parameters.count = 1000;
+parameters.size = 0.02;
+
+const generateGalaxy = () => {
+  //====================== Geometry
+  const galaxyGeometry = new THREE.BufferGeometry();
+  const positions = new Float32Array(parameters.count, 3);
+
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+    positions[i3] = (Math.random() - 0.5) * 10;
+    positions[i3 + 1] = (Math.random() - 0.5) * 10;
+    positions[i3 + 2] = (Math.random() - 0.5) * 10;
+  }
+
+  galaxyGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
+  );
+
+  //====================== Material
+  const galaxyMaterial = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+  });
+
+  //====================== Points
+  const galaxy = new THREE.Points(galaxyGeometry, galaxyMaterial);
+  scene.add(galaxy);
+};
+
+generateGalaxy();
 
 //====================== Camera =======================
 let width = window.innerWidth;
