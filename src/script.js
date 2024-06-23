cec8c6800311ac6eb9fdb611df7f2e6ea1f6eb15
ShahramShakiba <shahramshakiba88@gmail.com>
2024-06-23 14:29:08 +0430

    Shape the Galaxy with Radius and Branches

--- src/script.js
+++ src/script.js
@@ -10,6 +10,8 @@ const gui = new GUI().title('Galaxy Generator');
 const parameters = {};
 parameters.count = 100000;
 parameters.size = 0.01;
+parameters.radius = 5;
+parameters.branches = 3;
 
 let galaxyGeometry = null;
 let galaxyMaterial = null;
@@ -29,10 +31,14 @@ const galaxyGenerator = () => {
 
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
+    const radius = Math.random() * parameters.radius;
+    const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+    // Explained below ↓
 
-    positions[i3] = Math.random() - 0.5; // X
-    positions[i3 + 1] = Math.random() - 0.5; // Y
-    positions[i3 + 2] = Math.random() - 0.5; // Z
+    positions[i3] = Math.cos(branchAngle) * radius; // X
+    positions[i3 + 1] = 0; // Y
+    positions[i3 + 2] = Math.sin(branchAngle) * radius; // Z
   }
 
   galaxyGeometry.setAttribute(
@@ -55,11 +61,12 @@ const galaxyGenerator = () => {
 
 galaxyGenerator();
 
+//==== Debug GUI
 gui
   .add(parameters, 'count')
-  .min(100)
+  .min(300)
   .max(1000000)
-  .step(50)
+  .step(150)
   .name('Count')
   .onFinishChange(galaxyGenerator);
 gui
@@ -68,6 +75,20 @@ gui
   .max(0.1)
   .step(0.001)
   .name('Size')
+  .onFinishChange(galaxyGenerator);
+gui
+  .add(parameters, 'radius')
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .name('Radius')
+  .onFinishChange(galaxyGenerator);
+gui
+  .add(parameters, 'branches')
+  .min(2)
+  .max(15)
+  .step(1)
+  .name('Branches')
   .onFinishChange(galaxyGenerator);
 
 //====================== Camera =======================
@@ -117,3 +138,29 @@ const tick = () => {
 };
 
 tick();
+
+/* const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+- imagine this, I want to create 3 branches
+- I need to distribute "particles" only on this 3 branches equally
+
+- I'm going to use modular % 3
+- I get 0, 1, 2 and it never reaches 3
+- in this way it gets back to 0 
+
+- this goes for other quantity the same as above
+
+- then I like my values goes from "0" to "1", so I divide it by 3 
+- I get a range between [0, 0.33, 0.66], then it gets back to [0, 0.33, 0.66]
+
+- so it doesn't reaches to 1 because we don't want to have 2 branches in one place --- if it reaches 1, imagine it like this, the first branches is on 0, the second branches will be on its opposite position(180deg), then the third branches will reaches 0 or 360deg again --- so we will have 2 branches on 1 place
+
+
+- in the end, to have a full circle, I multiply PI by 2
+
+  - see how it works
+    if (i < 20) {
+      console.log(i, branchAngle);
+    }
+*/
